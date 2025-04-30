912f1f2e747f2a4a3359426a7ad413fdb6bf810d
Alec Fong <a9fong@ucsd.edu>
2025-04-29 20:24:05 -0700

    fixed bugs

--- scripts/lab4.js
+++ scripts/lab4.js
@@ -14,7 +14,7 @@ function sumValues(num1, num2, add) {
         return result;
     }
     else {
-        return !add;
+        return add;
     }
 }
 
@@ -29,9 +29,11 @@ function discountPrices(prices, discount) {
     const length = prices.length;
     let discountedPrice = 0
     for(let i = 0; i < length; i++) {
-        discountedPrice += prices[i] * (1 - discount);
+        discountedPrice = prices[i] * (1 - discount);
         discounted.push(discountedPrice);
     }
+
+    if(discounted.length == 0) return 0;
 
     return discounted;
 }
